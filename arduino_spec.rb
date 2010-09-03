de6f6c7b7af661242d34d22d6582092858a94677
Eric Metens <emetens@twitter.com>
2010-09-03 08:26:59 -0700

    adds spec for Arduino default options

--- arduino_spec.rb
+++ arduino_spec.rb
@@ -14,14 +14,21 @@ describe "Arduino" do
       lambda { Arduino.new }.should raise_error
     end
     
-    # it "merges the options with default options" do
-    #   arduino = Arduino.new :port => '/dev/tty.usbserial-A7006SoU'
-    #   pending
-    # end
+    it "should override default options with given options" do
+      options = {
+        :port => '/dev/tty.usbserial-A7006SoU',
+        :baud_rate => 999,
+        :data_bits => 998,
+        :stop_bits => 997,
+        :parity => SerialPort::NONE
+      }
+      mock(SerialPort).new(options[:port], options[:baud_rate], options[:data_bits], options[:stop_bits], options[:parity])
+      arduino = Arduino.new options      
+    end
     
-    it "open a connection to the specified serial port" do
+    it "open a connection to the specified serial port with the default options" do
       port = '/dev/tty.usbserial-A7006SoU'
-      mock(SerialPort).new(satisfy { |p| p == port }, 9600, 8, 1, 0)
+      mock(SerialPort).new(port, 9600, 8, 1, 0)
       arduino = Arduino.new(:port => port)
     end
   end
